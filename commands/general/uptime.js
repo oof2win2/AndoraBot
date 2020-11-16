21285102bb3af4c39b57d44fb2255a4b58130c39
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-11-16 20:54:21 +0100

    reformat

--- commands/general/uptime.js
+++ commands/general/uptime.js
@@ -1,14 +1,15 @@
 module.exports = {
   config: {
-    name: 'uptime',
-    aliases: ['ut'],
-    description: 'Get the bot\'s uptime',
-    category: 'general',
-    usage: '',
+    name: "uptime",
+    aliases: ["ut"],
+    description: "Get the bot's uptime",
+    category: "general",
+    usage: "",
+    accessibility: "members",
   },
   run: async (client, message) => {
     function formatTime(ms) {
-    // ms is divided to get the time desired and then remaindered to get the leftover
+      // ms is divided to get the time desired and then remaindered to get the leftover
       const s = Math.floor((ms / 1000) % 60).toString();
       const m = Math.floor((ms / (1000 * 60)) % 60).toString();
       const h = Math.floor((ms / (1000 * 60 * 60)) % 24).toString();
@@ -16,7 +17,10 @@ module.exports = {
       // string.padStart(length, filler)
       // pads the string with the $filler for the desired $length
       // if the length of the string is not equal to $length
-      return `\`${d.padStart(2, '0')}:${h.padStart(2, '0')}:${m.padStart(2, '0')}:${s.padStart(2, '0')}\``;
+      return `\`${d.padStart(2, "0")}:${h.padStart(2, "0")}:${m.padStart(
+        2,
+        "0"
+      )}:${s.padStart(2, "0")}\``;
     }
     message.channel.send(`My uptime: \`${formatTime(client.uptime)}\``);
   },
--- handlers/command.js
+++ handlers/command.js
@@ -1,20 +1,18 @@
 const fs = require(`fs`);
 
 module.exports = (client) => {
-    const load = (dirs) => {
-        const commands = fs.readdirSync(`./commands/${dirs}/`).filter((d) =>
-            d.endsWith(".js")
+  const load = (dirs) => {
+    const commands = fs
+      .readdirSync(`./commands/${dirs}/`)
+      .filter((d) => d.endsWith(".js"));
+    for (let file of commands) {
+      let pull = require(`../commands/${dirs}/${file}`);
+      client.commands.set(pull.config.name, pull);
+      if (pull.config.aliases)
+        pull.config.aliases.forEach((a) =>
+          client.aliases.set(a, pull.config.name)
         );
-        for (let file of commands) {
-            let pull = require(`../commands/${dirs}/${file}`);
-            client.commands.set(pull.config.name, pull);
-            if (pull.config.aliases)
-                pull.config.aliases.forEach((a) =>
-                    client.aliases.set(a, pull.config.name)
-                );
-        }
-    };
-    [
-        "general"
-    ].forEach(dir => load(dir));
-}
+    }
+  };
+  ["general"].forEach((dir) => load(dir));
+};
