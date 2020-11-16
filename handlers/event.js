708d2a58bde8dedf1bc2b2a0bb03de50db34cd33
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-11-16 20:54:21 +0100

    reformat

--- handlers/event.js
+++ handlers/event.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 
 module.exports = (client) => {
-    const load = (dir) => {
-        const events = fs.readdirSync(`./events/${dir}/`).filter((e) =>
-            e.endsWith(".js")
-        );
-        for (let file of events) {
-            const evt = require(`../events/${dir}/${file}`);
-            let eName = file.split(".")[0];
-            client.on(eName, evt.bind(null, client));
-        }
-    };
-    ["client", "guild"].forEach((e) => load(e));
+  const load = (dir) => {
+    const events = fs
+      .readdirSync(`./events/${dir}/`)
+      .filter((e) => e.endsWith(".js"));
+    for (let file of events) {
+      const evt = require(`../events/${dir}/${file}`);
+      let eName = file.split(".")[0];
+      client.on(eName, evt.bind(null, client));
+    }
+  };
+  ["client", "guild"].forEach((e) => load(e));
 };
